64431268d00f3517aeb8afead1bece05c0a83dd5
Arpitxd41 <Arpitnt10@gmail.com>
2024-04-05 01:18:46 +0530

    New modifications

--- server/server.js
+++ server/server.js
@@ -1,16 +1,9 @@
-const https = require('https');
-const fs = require('fs');
+const http = require('http'); // Change to http
 const app = require('./app.js');
 
 const PORT = process.env.PORT || 5000;
 
-const sslConfig = {
-    key: fs.readFileSync(process.env.SSL_KEY_PATH),
-    cert: fs.readFileSync(process.env.SSL_CERT_PATH),
-    passphrase: process.env.SSL_PASSPHRASE
-};
-
-const server = https.createServer(sslConfig, app);
+const server = http.createServer(app); // Change to http
 
 function startServer() {
     server.listen(PORT, () => {
