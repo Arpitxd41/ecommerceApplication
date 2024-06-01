e32fc29d466bde70ab0e7208db287b5685764a93
Arpitxd41 <arpitnt10@gmail.com>
2024-06-01 18:39:44 +0530

    fix: broken page

--- client/src/pages/error/fail.js
+++ client/src/pages/error/fail.js
@@ -18,17 +18,17 @@ const ErrorPage = () => {
       <div className="gif">
         <img src="https://i.postimg.cc/2yrFyxKv/giphy.gif" alt="gif_ing" />
       </div>
-      <div className="content">
+      <div className="content flex flex-row justify-center items-center w-full">
         <h1 className="main-heading">This page is not responding.</h1>
         <p className='statement'>
           ...maybe the page you're looking for is not found or never existed.
         </p>
         <div className='flex flex-col md:flex-row space-y-4 md:space-x-6 lg:w-2/3 w-full'>
           <button id='backhome' onClick={handleHomeClick} className='md:w-1/2'>
-            <i className="fa fa-sign-in"></i> LOGIN
+            <i className="fa fa-refresh"></i> SESSION
           </button>
           <button id='goback' onClick={handleBackClick} className='md:w-1/2'>
-            <i className="fa fa-home"></i> BACK
+            <i className="fa fa-backward"></i> BACK
           </button>
         </div>
       </div>
